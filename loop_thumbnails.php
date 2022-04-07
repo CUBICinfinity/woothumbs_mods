6c37732a03d5d125b66f8af53f82777761928bc7
∞³ Jim Greene <jimkgreene@gmail.com>
2022-04-07 10:44:46 -0600

    Allow product "loop" images to be customized.

--- loop_thumbnails.php
+++ loop_thumbnails.php
@@ -12,7 +12,7 @@
  * The featured product image will only appear in the woocommerce loop elements (shop pages, related products, etc.).
  * Only gallery images will be used on the product page. (The featured image is skipped.)
  * 
- * This change is not compatible with Woothumbs versions 2.11 and later because they deduplicate product images.
+ * This change is not compatible with Woothumbs versions 4.11 and later because they deduplicate product images.
  * However, after my complaints, Iconic to added a hook, `'iconic_woothumbs_dedupe_gallery_images'`, 
  * which will be of use getting this working in future versions of woothumbs.
  * Consider `add_filter( 'iconic_woothumbs_dedupe_gallery_images', '__return_false' );`
